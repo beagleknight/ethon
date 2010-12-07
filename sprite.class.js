06444741f01be3a1ccaa35789ca603ff269f718c
david <david@imesmes.com>
2010-12-07 19:38:14 +0100

    First implementation of pure Sprites
    Loading frames in one file is possible. Animation using event manager.

--- sprite.class.js
+++ sprite.class.js
@@ -2,33 +2,34 @@
   Class Sprite
 */
 
-Sprite.prototype = new Object2D();
-
-function Sprite(x,y,w,h) {
-  Object2D.call(this, x, y, w, h);
-  this.frames = new Array();
+function Sprite(w,h,speed) {
+  this.w = w;
+  this.h = h;
+  this.image = new Image();
   this.current = 0;
 
-  this.draw = function() {
-    //render.renderImage(this.frames[this.current], this.pos);
-    //Object2D.prototype.draw.call(this,render);
+  this.ethon = Ethon.getInstance();
+
+  this.ethon.event_manager.register('animation', function(event) {
+    event.set_time(speed);
+  });
+
+  this.draw = function(pos) {
+    var crop = new Vector2D();
+    crop.x = this.current*this.w;
+    crop.y = 0;
+    var dimension = new Vector2D(this.w, this.h);
+    Ethon.getInstance().render_manager.renderImage(this.image, pos, crop, dimension);
   };
 
-  this.update = function() {
-
+  this.update = function(dt) {
+    this.ethon.event_manager.update('animation',dt);
+    if(this.ethon.event_manager.happens('animation')) {
+      this.current = (this.current+1) % 5;    
+    }
   };
 
-  this.add_frame = function() {
-    //var img = new Image();
-    //img.src = url;
-    //this.frames.push(img);
-  };
-
-  this.current_frame = function() {
-    //return this.frames[this.current];
-  };
-
-  this.next_frame = function() {
-    //this.current = (this.current+1) % this.frames.length;    
+  this.load_frames = function(url) {
+    this.image.src = url;
   };
 };
