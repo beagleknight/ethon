2a36bb42a32d9803f7b54e4a49be08a2a4947319
david <david@imesmes.com>
2011-03-17 18:21:03 +0100

    Moved canvas collision to Object2D

--- public/javascripts/ethon/object_2d.class.js
+++ public/javascripts/ethon/object_2d.class.js
@@ -5,28 +5,30 @@
 var object_id = 0;
 
 function Object2D(x,y,w,h) {
-  //console.log('Object with id #'+id+' created.');
   this.id = object_id++;
   this.pos = new Vector2D(x,y);
   this.vel = new Vector2D(0,0);
   this.w = w;
   this.h = h;
+
   this.alive = true;
-  this.drawBB = false;
+  this.collideWithCanvas = true;
+  this.drawBB = true;
+
 
   this.draw = function() {
-    //if(this.drawBB) {
-    //  render.basicShape(BOX, this.pos, this.w, this.h);
-    //}
+    if(this.drawBB) {
+      Ethon.getInstance().render_manager.basicShape(BOX, this.pos, this.w, this.h);
+    }
   };
 
   this.update = function() {
     this.pos.sum(this.vel);
-  };
-
-  this.insideCanvas = function() {
-    //render = Game.getInstance().render;
-    //return (this.pos.x > 0) && (this.pos.x < render.canvas_width) && 
-    //(this.pos.y > 0) && (this.pos.y < render.canvas_height);
+    if(this.collideWithCanvas) {
+      if(this.pos.x+this.w > 700 || this.pos.x < 0)
+        this.vel.x = -this.vel.x;
+      if(this.pos.y+this.h > 700 || this.pos.y < 0)
+        this.vel.y = -this.vel.y;
+    }
   };
 };
