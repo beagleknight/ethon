f11488e699cc102aa8770e963725d56d66ea912b
ultrayoshi <david@imesmes.com>
2012-03-05 16:53:47 +0100

    Added some stubs to test methods

--- src/render_manager.js
+++ src/render_manager.js
@@ -4,6 +4,8 @@ ethon.render_manager = (function() {
       throw new TypeError("init must receive a CanvasElement");
     }
     this.canvas = canvas;
+    this.canvas.width = 500;
+    this.canvas.height= 500;
     this.ctx = this.canvas.getContext("2d");
   }
 
@@ -11,8 +13,13 @@ ethon.render_manager = (function() {
     this.ctx.fillRect(x, y, width, height);  
   }
 
+  function draw_circle(x, y, radius) {
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+  }
+
   return {
     init: init,
-    draw_box: draw_box 
+    draw_box: draw_box,
+    draw_circle: draw_circle 
   };
 })();
--- test/render_manager_test.js
+++ test/render_manager_test.js
@@ -1,4 +1,8 @@
 TestCase("RenderManagerTest", {
+  setUp: function() {
+    this.canvas = document.createElement('canvas');
+  },
+
   "test should be an object": function() {
     assertObject(ethon.render_manager);
   },
@@ -9,9 +13,23 @@ TestCase("RenderManagerTest", {
     }, "TypeError");
   },
 
-  "test draw box should use canvas context": function() {
-    var canvas = document.createElement('canvas');
-    ethon.render_manager.init(canvas);
+  "test draw box should use canvas context fillRect": function() {
+    var fillRect = stubFn();
+    this.canvas.getContext = stubFn({ fillRect: fillRect });
+
+    ethon.render_manager.init(this.canvas);
     ethon.render_manager.draw_box(25, 25, 100, 100);
+    assertTrue(fillRect.called);
+    assertEquals(4, fillRect.args.length);
+  },
+
+  "test draw circle should use canvas context arc": function() {
+    var arc = stubFn();
+    this.canvas.getContext = stubFn({ arc: arc });
+
+    ethon.render_manager.init(this.canvas);
+    ethon.render_manager.draw_circle(100, 100, 5);
+    assertTrue(arc.called);
+    assertEquals(6, arc.args.length);
   }
 });
