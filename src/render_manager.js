43be8f18841b3f27810612e9e6a1f3226de4ad00
ultrayoshi <david@imesmes.com>
2012-03-29 16:18:33 +0200

    Added style to drawBox and drawCircle functions

--- src/render_manager.js
+++ src/render_manager.js
@@ -1,26 +1,63 @@
 ethon.render_manager = (function() {
+  var canvas, ctx;
+  var fillStyle, strokeStyle;
+
   function init(canvas) {
     if(!(canvas instanceof HTMLCanvasElement)) {
       throw new TypeError("init must receive a CanvasElement");
     }
-    this.canvas = canvas;
-    this.canvas.width = 500;
-    this.canvas.height= 500;
-    this.ctx = this.canvas.getContext("2d");
+    canvas = canvas;
+    canvas.width = 500;
+    canvas.height= 500;
+    ctx = canvas.getContext("2d");
+
+    fillStyle = "#ffffff";
+    strokeStyle = "#000000";
   }
 
-  function draw_box(x, y, width, height) {
-    this.ctx.fillRect(x, y, width, height);  
+  function setDefaultStyle(options)
+  {
+    fillStyle = options.fillStyle;
+    strokeStyle = options.strokeStyle;
   }
 
-  function draw_circle(x, y, radius) {
-    this.ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
-    this.ctx.fill();
+  function getDefaultStyle()
+  {
+    return {
+      fillStyle: fillStyle,
+      strokeStyle: strokeStyle
+    };
+  }
+
+  function setStyle(options)
+  {
+    if(options != undefined) {
+      ctx.fillStyle = options.fillStyle;
+      ctx.strokeStyle = options.strokeStyle;
+    }
+    else {
+      ctx.fillStyle = fillStyle;
+      ctx.strokeStyle = strokeStyle;
+    }
+  }
+
+  function drawBox(x, y, width, height, options) {
+    setStyle(options);
+    ctx.fillRect(x, y, width, height);  
+  }
+
+  function drawCircle(x, y, radius, options) {
+    ctx.beginPath();
+    setStyle(options);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+    ctx.stroke();
   }
 
   return {
     init: init,
-    draw_box: draw_box,
-    draw_circle: draw_circle 
+    setDefaultStyle: setDefaultStyle,
+    getDefaultStyle: getDefaultStyle,
+    drawBox: drawBox,
+    drawCircle: drawCircle 
   };
 })();
--- test/render_manager_test.js
+++ test/render_manager_test.js
@@ -14,30 +14,100 @@ describe("RenderManager", function() {
     });
   });
 
-  describe("draw_box", function() {
-    it("should call canvas context fillRect with correct arguments", function() {
-      spyOn(ctx, 'fillRect');
-      ethon.render_manager.init(canvas);
-      ethon.render_manager.draw_box(25, 25, 100, 100);
-      expect(ctx.fillRect).toHaveBeenCalledWith(25, 25, 100, 100);
+  describe("setDefaultStyle", function() {
+    it("should assign default styles", function() {
+      ethon.render_manager.setDefaultStyle({
+        fillStyle: "#00ff00",
+        strokeStyle: "#ff0000"
+      });
+      var defaultStyle = ethon.render_manager.getDefaultStyle();
+      expect(defaultStyle.fillStyle).toBe("#00ff00");
+      expect(defaultStyle.strokeStyle).toBe("#ff0000");
     });
   });
 
-  describe("draw_circle", function() {
+  describe("drawBox", function() {
+    it("should call canvas context fillRect with correct arguments", function() {
+      spyOn(ctx, 'fillRect');
+      ethon.render_manager.init(canvas);
+      ethon.render_manager.drawBox(25, 25, 100, 100);
+      expect(ctx.fillRect).toHaveBeenCalledWith(25, 25, 100, 100);
+    });
+
+    describe("with default style arguments", function() {
+      it("should take manager default fillStyle value", function() {
+        spyOn(ctx, 'fillStyle');
+        ethon.render_manager.init(canvas);
+        ethon.render_manager.setDefaultStyle({
+          fillStyle: "#ff0000"
+        });
+        ethon.render_manager.drawBox(25, 25, 100, 100);
+        expect(ctx.fillStyle).toBe("#ff0000");
+      });
+    });
+
+    describe("with explicit fillStyle argument", function() {
+      it("should use fillStyle argument value", function() {
+        spyOn(ctx, 'fillStyle');
+        ethon.render_manager.init(canvas);
+        ethon.render_manager.setDefaultStyle({
+          fillStyle: "#ff0000"
+        });
+        ethon.render_manager.drawBox(25, 25, 100, 100, {
+          fillStyle: "#00ff00"
+        });
+        expect(ctx.fillStyle).toBe("#00ff00");
+      });
+    });
+  });
+
+  describe("drawCircle", function() {
     beforeEach(function () {
       ethon.render_manager.init(canvas);
     });
 
+    it("should begin a new path for canvas context", function() {
+      spyOn(ctx, 'beginPath');
+      ethon.render_manager.drawCircle(100, 100, 5);
+      expect(ctx.beginPath).toHaveBeenCalled();
+    });
+
     it("should call canvas context arc with correct arguments", function() {
       spyOn(ctx, 'arc');
-      ethon.render_manager.draw_circle(100, 100, 5);
+      ethon.render_manager.drawCircle(100, 100, 5);
       expect(ctx.arc).toHaveBeenCalledWith(100, 100, 5, 0, 2 * Math.PI, true);
     });
 
-    it("should call canvas context fill", function() {
-      spyOn(ctx, 'fill');
-      ethon.render_manager.draw_circle(100, 100, 5);
-      expect(ctx.fill).toHaveBeenCalled();
+    it("should call canvas context stroke", function() {
+      spyOn(ctx, 'stroke');
+      ethon.render_manager.drawCircle(100, 100, 5);
+      expect(ctx.stroke).toHaveBeenCalled();
+    });
+
+    describe("with default style arguments", function() {
+      it("should take manager default strokeStyle value", function() {
+        spyOn(ctx, 'strokeStyle');
+        ethon.render_manager.init(canvas);
+        ethon.render_manager.setDefaultStyle({
+          strokeStyle: "#ff0000"
+        });
+        ethon.render_manager.drawCircle(100, 100, 5);
+        expect(ctx.strokeStyle).toBe("#ff0000");
+      });
+    });
+
+    describe("with explicit strokeStyle argument", function() {
+      it("should use strokeStyle argument value", function() {
+        spyOn(ctx, 'strokeStyle');
+        ethon.render_manager.init(canvas);
+        ethon.render_manager.setDefaultStyle({
+          strokeStyle: "#ff0000"
+        });
+        ethon.render_manager.drawCircle(100, 100, 5, {
+          strokeStyle: "#0000ff"
+        });
+        expect(ctx.strokeStyle).toBe("#0000ff");
+      });
     });
   });
 });
