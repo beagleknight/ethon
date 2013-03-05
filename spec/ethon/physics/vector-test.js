faa0c6d3e8c67a7a157d1d9dc2d42b099ab5809b
ultrayoshi <david@imesmes.com>
2013-03-05 18:57:26 +0100

    Add a few methods for adding and substracting operations

--- spec/ethon/physics/vector-test.js
+++ spec/ethon/physics/vector-test.js
@@ -12,64 +12,97 @@ describe("Vector", function (run) {
         run(function () {
             describe("constructor", function () {
                 it("initialize coordinates to origin", function () {
-                    var newVec = new Vector();
-                    expect(newVec.x).toBe(0);
-                    expect(newVec.y).toBe(0);
+                    var vec = new Vector();
+                    expect(vec.x).toBe(0);
+                    expect(vec.y).toBe(0);
                 });
 
                 it("initialize coordinates to given arguments", function () {
-                    var newVec = new Vector(3, 4);
+                    var vec = new Vector(3, 4);
                     
-                    expect(newVec.x).toBe(3);
-                    expect(newVec.y).toBe(4);
+                    expect(vec.x).toBe(3);
+                    expect(vec.y).toBe(4);
                 });
             });
 
             describe("invert", function () {
                 it("flips all the components of the vector", function () {
-                    var newVec = new Vector(3, 4);
-                    newVec.invert();
-                    expect(newVec.x).toBe(-3);
-                    expect(newVec.y).toBe(-4);
+                    var vec = new Vector(3, 4);
+                    vec.invert();
+                    expect(vec.x).toBe(-3);
+                    expect(vec.y).toBe(-4);
                 });
             });
 
             describe("magnitude", function () {
                 it("returns vector's magnitude", function () {
-                    var newVec = new Vector(3, 4);
-                    expect(newVec.magnitude()).toBe(5);
+                    var vec = new Vector(3, 4);
+                    expect(vec.magnitude()).toBe(5);
                 });
             });
 
             describe("squareMagnitude", function () {
                 it("returns vector's squared magnitude", function () {
-                    var newVec = new Vector(3, 4);
-                    expect(newVec.squaredMagnitude()).toBe(25);
+                    var vec = new Vector(3, 4);
+                    expect(vec.squaredMagnitude()).toBe(25);
                 });
             });
 
             describe("normalize", function () {
                 it("convert vector to its corresponding unit lenghth vector", function () {
-                    var newVec = new Vector(3, 4);
-                    newVec.normalize();
-                    expect(newVec.x).toBe(0.6);
-                    expect(newVec.y).toBe(0.8);
+                    var vec = new Vector(3, 4);
+                    vec.normalize();
+                    expect(vec.x).toBe(0.6);
+                    expect(vec.y).toBe(0.8);
                 });
 
                 it("doesn't touch a 0 length vector", function () {
-                    var newVec = new Vector();
-                    newVec.normalize();
-                    expect(newVec.x).toBe(0);
-                    expect(newVec.y).toBe(0);
+                    var vec = new Vector();
+                    vec.normalize();
+                    expect(vec.x).toBe(0);
+                    expect(vec.y).toBe(0);
                 });
             });
 
             describe("multScalar", function () {
                 it("returns a new vector result of multiply vector by the scalar", function () {
-                    var newVec = new Vector(3, 4),
-                        resultVec = newVec.multScalar(2);
+                    var vec = new Vector(3, 4),
+                        resultVec = vec.multScalar(2);
                     expect(resultVec.x).toBe(6);
                     expect(resultVec.y).toBe(8);
+                });
+            });
+
+            describe("add", function () {
+                it("returns a new vector result of adding vectors", function () {
+                    var vec1 = new Vector(3, 4),
+                        vec2 = new Vector(1, 5),
+                        resultVec = vec1.add(vec2);
+                    
+                    expect(resultVec.x).toBe(4);
+                    expect(resultVec.y).toBe(9);
+                });
+            });
+
+            describe("sub", function () {
+                it("returns a new vector result of substracting vectors", function () {
+                    var vec1 = new Vector(3, 4),
+                        vec2 = new Vector(1, 5),
+                        resultVec = vec1.sub(vec2);
+                    
+                    expect(resultVec.x).toBe(2);
+                    expect(resultVec.y).toBe(-1);
+                });
+            });
+
+            describe("addScaledVector", function () {
+                it("perfomrs a double operation scaling given vector and adding it", function () {
+                    var vec1 = new Vector(3, 4),
+                        vec2 = new Vector(1, 5),
+                        resultVec = vec1.addScaledVector(vec2, 2);
+                    
+                    expect(resultVec.x).toBe(5);
+                    expect(resultVec.y).toBe(14);
                 });
             });
         });
