33b1ca68222e290bf684bf1ee912d08187f66a95
ultrayoshi <david@imesmes.com>
2013-06-28 11:45:08 +0200

    Refactor ActionDispatcher tests

--- test/ethon/action_dispatcher_test.js
+++ test/ethon/action_dispatcher_test.js
@@ -1,7 +1,12 @@
 define(['jquery', 'ethon/input_assistant', 'ethon/action_dispatcher'], function ($, InputAssistant, ActionDispatcher) {
-    var canvas, canvasRect, inputAssistant, actionDispatcher;
+    var testValue,
+        canvas,
+        canvasRect,
+        inputAssistant,
+        actionDispatcher;
 
     beforeEach(function () {
+        testValue = '';
         canvas = document.createElement('canvas');
         canvas.width = 640;
         canvas.height = 480;
@@ -14,31 +19,27 @@ define(['jquery', 'ethon/input_assistant', 'ethon/action_dispatcher'], function 
     describe("ActionDispatcher", function () {
         describe("#registerKeyboardAction", function () {
             it("should register a callback method in response to keydown event", function () {
-                var testValue = '';
-
                 actionDispatcher.registerKeyboardAction("KEY_A", function () {
                     testValue = 'A key pressed';
                 });
 
-                var eventData = new jQuery.Event();
-                eventData.keyCode = 65; // A key
-                eventData.type = "keydown";
-                $('body').trigger(eventData);
+                triggerEvent('body', {
+                    keyCode: 65, // A key
+                    type: "keydown",
+                });
 
                 expect(testValue).toBe('A key pressed');
             });
 
             it("accepts a second callback method in response to keyup event", function () {
-                var testValue = '';
-
                 actionDispatcher.registerKeyboardAction("KEY_A", function () {}, function () {
                     testValue = 'A key released';
                 });
 
-                var eventData = new jQuery.Event();
-                eventData.keyCode = 65; // A key
-                eventData.type = "keyup";
-                $('body').trigger(eventData);
+                triggerEvent('body', {
+                    keyCode: 65, // A key
+                    type: "keyup",
+                });
 
                 expect(testValue).toBe('A key released');
             });
@@ -46,17 +47,15 @@ define(['jquery', 'ethon/input_assistant', 'ethon/action_dispatcher'], function 
 
         describe("#registerMouseMotionAction", function () {
             it("should register a callback method in response to mouse motion inside a quad", function () {
-                var testValue = '';
-
                 actionDispatcher.registerMouseMotionAction({ x: 0, y: 0, w: 400, h: 400 }, function () {
                     testValue = 'Mouse motion inside canvas';
                 });
 
-                var eventData = new jQuery.Event();
-                eventData.clientX = 200 + canvasRect.left;
-                eventData.clientY = 300 + canvasRect.top;
-                eventData.type = "mousemove";
-                $(canvas).trigger(eventData);
+                triggerEvent(canvas, {
+                    clientX: 200,
+                    clientY: 300,
+                    type: "mousemove",
+                });
 
                 expect(testValue).toBe('Mouse motion inside canvas');
             });
@@ -64,18 +63,16 @@ define(['jquery', 'ethon/input_assistant', 'ethon/action_dispatcher'], function 
 
         describe("#registerMouseClickAction", function () {
             it("should register a callback method in response to mouse click inside a quad", function () {
-                var testValue = '';
-
                 actionDispatcher.registerMouseClickAction("MOUSE_LEFT", { x: 0, y: 0, w: 400, h: 400 }, function () {
                     testValue = 'Mouse click inside canvas';
                 });
 
-                var eventData = new jQuery.Event();
-                eventData.clientX = 200 + canvasRect.left;
-                eventData.clientY = 300 + canvasRect.top;
-                eventData.type = "mousedown";
-                eventData.button = 0; // Left button
-                $(canvas).trigger(eventData);
+                triggerEvent(canvas, {
+                    clientX: 200,
+                    clientY: 300,
+                    type: "mousedown",
+                    button: 0 // Left button
+                });
 
                 expect(testValue).toBe('Mouse click inside canvas');
             });
@@ -88,4 +85,14 @@ define(['jquery', 'ethon/input_assistant', 'ethon/action_dispatcher'], function 
     afterEach(function () {
         $(canvas).remove();
     });
+
+    function triggerEvent(element, options) {
+        var eventData = new jQuery.Event();
+        eventData.clientX = options.clientX + canvasRect.left;
+        eventData.clientY = options.clientY + canvasRect.top;
+        eventData.type = options.type;
+        eventData.button = options.button;
+        eventData.keyCode = options.keyCode;
+        $(element).trigger(eventData);
+    }
 });
