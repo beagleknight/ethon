0a50ee5b2c4ab17fcfdc35e01f5441d08799d31f
ultrayoshi <david@imesmes.com>
2013-07-12 14:34:22 +0200

    Add extend to main script

--- src/ethon/main.js
+++ src/ethon/main.js
@@ -1,6 +1,6 @@
 /*jslint browser: true*/
 /*global require*/
 
-require(["ethon/game", "ethon/scene", "ethon/soul", "ethon/sprite", "ethon/resource_assistant"], function () {
+require(["ethon/game", "ethon/scene", "ethon/soul", "ethon/sprite", "ethon/resource_assistant", "ethon/extend"], function () {
     "use strict";
 });
