28b4aef59b8f4ede8ef1dbc6d277316f40cf78a6
ultrayoshi <david@imesmes.com>
2013-07-18 10:09:29 +0200

    Add Firefox to browsers in karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome', 'PhantomJS'];
+browsers = ['Chrome', 'Firefox', 'PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
