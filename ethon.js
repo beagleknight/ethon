d3f67bbf45483740eff311fe915958f66e2e045a
ultrayoshi <david@imesmes.com>
2013-12-24 10:49:19 +0100

    Add particle system to main build

--- ethon.js
+++ ethon.js
@@ -2286,7 +2286,158 @@ define('ethon/extend',[],function () {
     };
 });
 
-require(["ethon/game", "ethon/sprite", "ethon/extend"], function () {
+define('ethon/particle',['require','ethon/soul','ethon/inherit'],function (require) {
+    
+
+    var Soul      = require("ethon/soul"),
+        inherit   = require("ethon/inherit"),
+        Particle;
+
+    Particle = function (options) {
+        var position = options.position || { x: 0, y: 0 };
+
+        Soul.call(this, "Particle", position.x, position.y);
+
+        this.color  = options.color;
+        this.size   = options.size;
+        this.forces = options.forces;
+    };
+
+    inherit(Particle, Soul);
+
+    Particle.prototype.render = function (renderAssistant) {
+        var ctx = renderAssistant.getContext();
+
+        ctx.save();
+        ctx.fillStyle = this.color;
+        ctx.globalAlpha = this.life / this.maxLife;
+        ctx.translate(this.position.x, this.position.y);
+        ctx.beginPath();
+        ctx.arc(0, 0, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.restore();
+    };
+
+    Particle.prototype.update = function (dt) {
+        var i, l;
+
+        for (i = 0, l = this.forces.length; i < l; i += 1) {
+            this.position.x += this.forces[i].x * dt;
+            this.position.y += this.forces[i].y * dt;
+        }
+
+        this.life -= dt;
+        if (this.life < 0) {
+            this.life = 0;
+        }
+    };
+
+    Particle.prototype.setLife = function (life) {
+        this.maxLife = life;
+        this.life = life;
+    };
+
+    return Particle;
+});
+
+define('ethon/particle_system',['require','ethon/inherit','ethon/soul','ethon/particle'],function (require) {
+    
+    
+    var inherit  = require("ethon/inherit"),
+        Soul     = require("ethon/soul"),
+        Particle = require("ethon/particle"),
+        ParticleSystem;
+
+    function valueOrDefault(value, _default) {
+        if (value === null || value === undefined) {
+            return _default;
+        }
+        return value;
+    }
+
+    function random (min, max) {
+        return min + (~~(Math.random() * (max - min + 1)));
+    }
+
+    ParticleSystem = function (options) {
+        var position      = valueOrDefault(options.position, { x: 0, y: 0 }),
+            nParticles    = valueOrDefault(options.nParticles, 10),
+            particleColor = valueOrDefault(options.particleColor, "#ffffff"),
+            i,
+            particle;
+
+        Soul.call(this, "ParticleSystem", position.x, position.y);
+
+        this.particles            = [];
+        this.particleRespawn      = valueOrDefault(options.particleRespawn, false);
+        this.minParticleSize      = valueOrDefault(options.minParticleSize, 1);
+        this.maxParticleSize      = valueOrDefault(options.maxParticleSize, 5);
+        this.minParticleLifeTime  = valueOrDefault(options.minParticleLifeTime, 1);
+        this.maxParticleLifeTime  = valueOrDefault(options.maxParticleLifeTime, 4);
+        this.minParticleVelocityX = valueOrDefault(options.minParticleVelocityX, -50);
+        this.maxParticleVelocityX = valueOrDefault(options.maxParticleVelocityX, 50);
+        this.minParticleVelocityY = valueOrDefault(options.minParticleVelocityY, -50);
+        this.maxParticleVelocityY = valueOrDefault(options.maxParticleVelocityY, 50);
+        this.particleForces       = valueOrDefault(options.particleForces, []);
+
+        for (i = 0; i < nParticles; i += 1) {
+            particle = new Particle({
+                color: particleColor
+            });
+            this.setParticleState(particle);
+            this.particles.push(particle);
+        }
+    };
+    inherit(ParticleSystem, Soul);
+
+    ParticleSystem.prototype.render = function (renderAssistant) {
+        var i, l;
+
+        for (i = 0, l = this.particles.length; i < l; i += 1) {
+            this.particles[i].render(renderAssistant);
+        }
+    };
+
+    ParticleSystem.prototype.update = function (dt) {
+        var i, l, deadParticles = [];
+
+        for (i = 0, l = this.particles.length; i < l; i += 1) {
+            this.particles[i].update(dt);
+
+            if (this.particles[i].life <= 0) {
+                if (this.particleRespawn) {
+                    this.setParticleState(this.particles[i]);
+                } else {
+                    deadParticles.push(i);
+                }
+            }
+        }
+
+        for (i = 0, l = deadParticles.length; i < l; i += 1) {
+            this.particles.splice(deadParticles[i], 1);
+        }
+
+        if (this.particles.length === 0) {
+            this.remove = true;
+        }
+    };
+
+    ParticleSystem.prototype.setParticleState = function (particle) {
+        particle.position = { x: this.position.x, y: this.position.y };
+        particle.size = random(this.minParticleSize, this.maxParticleSize);
+        particle.setLife(random(this.minParticleLifeTime, this.maxParticleLifeTime));
+        particle.forces =  [
+            { 
+                x: random(this.minParticleVelocityX, this.maxParticleVelocityX), 
+                y: random(this.minParticleVelocityY, this.maxParticleVelocityY)
+            }
+        ].concat(this.particleForces);
+    };
+
+    return ParticleSystem;
+});
+
+require(["ethon/game", "ethon/sprite", "ethon/extend", "ethon/particle_system"], function () {
     
 });
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-require(["ethon/game", "ethon/sprite", "ethon/extend"], function () {
+require(["ethon/game", "ethon/sprite", "ethon/extend", "ethon/particle_system"], function () {
     "use strict";
 });
