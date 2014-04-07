8ad29785ae8727a4e56a993892efb8aa71f34467
ultrayoshi <david@imesmes.com>
2014-04-07 09:35:50 +0200

    Browserify migration complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 (function () {
     module.exports = {
-        Game: require('./game')
+        Game: require('./game'),
+        resourceAssistant: require('./resource_assistant'),
+        proxy: require('./proxy'),
+        ParticleSystem: require('./particle_system'),
+        Soul: require('./soul'),
+        inherit: require('./inherit'),
+        QuadBody: require('./quad_body'),
+        EventEmitter: require('./event_emitter')
     };
 }());
